f46f3ddf30b3c87003501e5cf2e4f7cafae66c13
Stuart Luscombe <sluscombe@saintsfc.co.uk>
2019-06-18 14:25:52 +0100

    Finish user edit, update, index, and destroy actions

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -2,6 +2,10 @@ require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
   
+  def setup
+    @user = users(:michael)
+  end
+
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -50,4 +50,20 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     log_in_as(@user, remember_me: '0')
     assert_empty cookies[:remember_token]
   end
+
+  test "test layout links" do
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", root_path, count:2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", signup_path
+    
+    get contact_path
+    assert_select "title", full_title("Contact")
+
+    get signup_path
+    assert_select "title", full_title("Sign up")
+  end
 end
